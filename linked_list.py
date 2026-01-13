2d8e82e2f92a4bd1b01c11f1d20614ff47363bc5
wesblake@proton.me <wesblake@proton.me>
2026-01-12 17:02:05 -0800

    playing with linked_list

---
+++ linked_list.py
@@ -0,0 +1,36 @@
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    def __repr__(self):
+        return str(self.data)
+
+class LinkedList:
+    def __init__(self, node = None):
+        self.head = None
+    def __repr__(self):
+        node = self.head
+        result = ""
+        while node != None:
+            result += str(node.data) + " -> "
+            node = node.next
+        else:
+            result += "None"
+        return result
+    def add(self, node):
+        cnode = self.head
+        while cnode != None:
+            if cnode.next == None:
+                cnode.next = Node(node)
+            else:
+                cnode = cnode.next
+
+
+if __name__ == "__main__":
+    llist = LinkedList([1,2,3])
+    llist.add(24)
+    llist.add("this")
+    llist.add([])
+    llist.add({"test": 1, "test": 2})
+    print(llist)
