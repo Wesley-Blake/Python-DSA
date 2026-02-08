c0f9be46280a03f2929a55eb657fef69edc10e17
= <=>
2026-02-07 17:32:13 -0800

    Completeing the lesson on hashtables.

--- hashing/hash_table.py
+++ hashing/hash_table.py
@@ -1,5 +1,39 @@
+from typing import NamedTuple, Any
+
+class Pair(NamedTuple):
+    key: Any
+    value: Any
+
 class HashTable:
-    def __init__(self, size):
-        self.size = size
+    def __init__(self, capacity):
+        self.pairs = capacity * [None]
     def __len__(self):
-        return self.size
+        return len(self.pairs)
+    def __setitem__(self, key, value):
+        self.pairs[self._index(key)] = Pair(key, value)
+    def __getitem__(self, key):
+        pair = self.pairs[self._index(key)]
+        if pair is None:
+            raise KeyError(key)
+        else:
+            return pair.value
+    def __delitem__(self, key):
+        if key in self:
+            self.pairs[self._index(key)] = None
+        else:
+            raise KeyError(key)
+    def __contains__(self, key):
+        try:
+            self[key]
+        except KeyError:
+            return False
+        else:
+            return True
+    def get(self, key, default = None):
+        try:
+            return self[key]
+        except:
+            self[key] = default
+            return self[key]
+    def _index(self, key):
+        return hash(key) % len(self)
