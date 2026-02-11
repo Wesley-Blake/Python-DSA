45c02f53e364edaacaadfa57dfb6b58cac50554f
wesblake@proton.me <wesblake@proton.me>
2026-02-10 17:03:04 -0800

    Added more to head creation test.

--- heap_stack_queue/test_heap.py
+++ heap_stack_queue/test_heap.py
@@ -4,3 +4,6 @@ def test_heap_heapify():
     heap_heapify = Heap()
     heap_heapify.heapify([1,2,3])
     assert len(heap_heapify._heap) == 3
+def test_fail_heap_heapify():
+    failed_heap = Heap()
+    failed_heap.heapify(1)
